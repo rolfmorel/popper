0c78767a99206405a0ef45ff86134c992699a50d
Andrew Cropper <andrew.cropper@gmail.com>
2020-03-25 13:58:04 +0000

    fixed bug

--- popper/constrain/generalization.py
+++ popper/constrain/generalization.py
@@ -8,6 +8,6 @@ def generalization_constraint(program):
     for cl_id, clause in enumerate(program):
         clause_var = next(clause_id_var_gen)
         for atom in clause:
-            gen_constaint += atom_to_asp_literals(atom, clause_var)
-        gen_constraint.append(f"size({clause_var},{len(clause)-1})") 
+            gen_constraint += atom_to_asp_literals(atom, clause_var)
+        gen_constraint.append(f"size({clause_var},{len(clause)-1})")
     return ":-" +  ",".join(gen_constraint) + "."
