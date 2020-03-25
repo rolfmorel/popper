064e98d634c1af865d1331b28128e96515a7e70d
polyphemus <rolfmorel@gmail.com>
2020-03-25 01:21:19 +0100

    Fix elimination constraints

--- popper/constrain/elimination.py
+++ popper/constrain/elimination.py
@@ -5,8 +5,8 @@ def elimination_constraint(program):
     elim_constraint = []
     for cl_id, clause in enumerate(program):
         for atom in clause:
-            elim_constaint += atom_to_asp_literals(atom)
+            elim_constraint += atom_to_asp_literals(atom)
         num_lits = len(clause)
-        elim_constraint.append(f"not literal({clause_id},{num_lits},_,_)")
+        elim_constraint.append(f"not literal({cl_id},{num_lits},_,_)")
     elim_constraint.append(f"not clause({len(program)})")
-    return ":- " + ",".join(spec_constraint) + "."
+    return ":- " + ",".join(elim_constraint) + "."
