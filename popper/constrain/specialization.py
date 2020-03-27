50fcdaa3eae16d41d3d8af1fae008a34d0cc5f94
polyphemus <rolfmorel@gmail.com>
2020-03-27 16:15:27 +0100

    Ensure body lits have index > 0 for non-ground constraints

--- popper/constrain/specialization.py
+++ popper/constrain/specialization.py
@@ -14,8 +14,11 @@ def specialization_constraint(program, ground=True):
         spec_constraint += lits
 
         for atom in body:
+            lit_var = next(lit_id_var_gen)
             lits = atom_to_asp_literals(atom, not ground and clause_var,
-                                        not ground and next(lit_id_var_gen))
+                                        not ground and lit_var)
             spec_constraint += lits
+            if not ground:
+                spec_constraint += [f"{lit_var} > 0"]
     spec_constraint.append(f"not clause({len(program)})")
     return ":- " + ",".join(spec_constraint) + "."
