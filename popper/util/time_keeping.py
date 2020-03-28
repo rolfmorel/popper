fb4b24a29775828b6317342cc498520acc0bc4cf
polyphemus <rolfmorel@gmail.com>
2020-03-28 21:19:51 +0100

    Pause and unpause parent
    Note that there is an issue with tracking mean(s) when a context has
    been paused.

--- popper/util/time_keeping.py
+++ popper/util/time_keeping.py
@@ -1,32 +1,66 @@
+from functools import reduce
 from timeit import default_timer as timer
 
 
+#def timed_method(decorated_func, context_names):
+#    def wrapped(self, *args, **kwargs):
+#        accumulator = None
+#        for name in context_names:
+#            accumulator = self.context._sub_accs[name]
+#        return decorated_func(self, *args, **kwargs)
+#    return wrapped
+
+
 class TimeAccumulatingContext(object):
-    def __init__(self):
-        self._accumulated = 0
-        self._sub_accs = {}
-        self.mean = 0
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.children = {}
         self.times_entered = 0
         self._times = []
 
-    def sub_accumulator(self, name):
-        sub_acc = __class__()
-        setattr(self, name, sub_acc)
-        self._sub_accs[name] = sub_acc
-        return sub_acc
+        self.currently_entered = 0
+        self.paused = False
+
+    @property
+    def running(self):
+        return self.currently_entered != 0
+
+    def add_child(self, name):
+        child = __class__(parent=self)
+        setattr(self, name, child)
+        self.children[name] = child
+        return child
+
+    def pause(self):
+        if self.running and not self.paused:
+            self.exit()
+            if self.parent: self.parent.pause()
+            self.paused = True
+
+    def unpause(self):
+        any_running_children = reduce(lambda acc, child: child.running or acc,
+                                      self.children.values(), False)
+        if self.paused and not any_running_children:
+            self.paused = False
+            self.enter()
+            self.times_entered -= 1
 
     def __enter__(self):
-        self.entered = timer()
+        self.currently_entered += 1
+        assert self.currently_entered == 1
         self.times_entered += 1
+        if self.parent: self.parent.pause()
+        self.entered = timer()
         return self
     enter = __enter__
 
     def __exit__(self, type, value, traceback):
+        self.currently_entered -= 1
+        assert self.currently_entered == 0
         self.exited = timer()
+        if self.parent: self.parent.unpause()
+
         diff = self.exited - self.entered
-        n = self.times_entered
-        self.mean = self.mean * (n - 1) / n + diff / n
-        self._accumulated += diff
         self._times.append(diff)
     def exit(self):
         self.__exit__(None, None, None)
@@ -34,18 +68,25 @@ class TimeAccumulatingContext(object):
 
     @property
     def accumulated(self):
-        return self._accumulated + sum(acc.accumulated for acc in self._sub_accs.values())
+        return sum(self._times) + sum(child.accumulated for child in self.children.values())
 
     def as_dict(self):
-        d = { k: v.as_dict() for k, v in self._sub_accs.items() }
+        d = { k: v.as_dict() for k, v in self.children.items() }
         d['_total'] = self.accumulated
-        d['_mean'] = self.mean
-        d['_means'] = self.means
+        if not self.children:
+            d['_mean'] = self.mean
+            d['_means'] = self.means 
         d['_times_entered'] = self.times_entered
         return d
 
     @property
+    def mean(self):
+        # FIXME: due to pausing this is no longer accurate! Fine for leaf contexts though
+        return sum(self._times) / len(self._times) if self._times else 0
+
+    @property
     def means(self, n=20):
+        # FIXME: due to pausing this is no longer accurate! Fine for leaf contexts though
         def chunks(lst, n):
             """Yield successive n-sized chunks from lst."""
             for i in range(0, len(lst), n):
