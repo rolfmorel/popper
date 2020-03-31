932537e8c262fccc78b05f797d47d9b692abc139
polyphemus <rolfmorel@gmail.com>
2020-03-31 18:35:23 +0200

    New program representation, their data types and their parsing

--- popper/representation.py
+++ popper/representation.py
@@ -1,25 +1,38 @@
 import re
 
 from enum import Enum
+from collections import namedtuple
 
 from functools import reduce
 
 
-EXAMPLE_PROGRAM = [[(0,0,'f',[0,1]),(0,1,'tail',[0,2]),(0,2,'head',[2,1])],
-                   [(1,0,'f',[0,1]),(1,1,'reverse',[0,2]),(1,2,'sumlist',[2,1])],
-                   ]
+#EXAMPLE_PROGRAM = [(0, Atom('f',mode,[0,1]),{Atom('tail',mode,[0,2]),Atom('head',mode,[2,1])})],
+#                   (1, Atom('f',mode,[0,1]),{Atom('reverse',mode,[0,2]),Atom('sumlist',mode,[2,1])})],
+
+#                   ]
+# where mode is an appropriate mode for each atom
+
+
+class Atom(namedtuple('Atom', ['predicate', 'mode', 'arguments'])):
+     @property
+     def arity(self):
+         return len(self.arguments)
+
+     def __str__(self):
+         return f"{self.predicate}({','.join(self.arguments)})"
+
+     def __repr__(self): # strictly speaking wrong, but more useful for debugging
+         mode_args = (f"{m.value}V{arg}" for arg, m in zip(self.arguments, self.mode.argument_modes))
+         return f"{self.predicate}({','.join(mode_args)})"
 
 
 class ArgumentMode(Enum):
     Input = '+'
     Output = '-'
+    Unknown = '?'
 
 
-class ModeDecleration(object):
-    def __init__(self, predicate, argument_modes):
-        self.predicate = predicate
-        self.argument_modes = argument_modes
-
+class ModeDecleration(namedtuple('ModeDecleration', ['predicate', 'argument_modes'])):
     @property
     def arity(self):
         return len(self.argument_modes)
@@ -29,21 +42,35 @@ class ModeDecleration(object):
         return self.predicate + "(" + ",".join("-" if mode == Out else "+" for mode in self.argument_modes) + ")"
 
     @staticmethod
-    def from_str(str_):
-        result = re.search("modeh\((.*),(\d+)\)", str_)
-        predicate, arity = result.group(1), result.group(2)
+    def parse_direction_modes(str_, predicate, arity):
         argument_modes = []
         for argument_idx in range(int(arity)):
-            result = re.search(f"direction\({predicate},{argument_idx},(.*)\)", str_)
+            result = re.search(f"direction\({predicate}, *{argument_idx}, *([^\n ]*)\).", str_)
             if result and result.group(1) == 'out':
                 argument_modes.append(ArgumentMode.Output)
-            else: # NB, this case is uses as a default as well
+            elif result and result.group(1) == 'in':
                 argument_modes.append(ArgumentMode.Input)
-        return __class__(predicate, argument_modes)
+            else: # NB, this case is used as a default as well
+                argument_modes.append(ArgumentMode.Unknown)
+        return __class__(predicate, tuple(argument_modes))
+
+    @staticmethod
+    def from_modeh(str_):
+        result = re.search("modeh\(([^,]*?), *(\d+)\).", str_)
+        predicate, arity = result.group(1), result.group(2)
+        return __class__.parse_direction_modes(str_, predicate, arity)
+
+    @staticmethod
+    def from_modebs(str_):
+        modebs = []
+        for result in re.findall("modeb\(([^,\n]*?), *(\d+)\).", str_):
+            predicate, arity = result
+            modebs.append(__class__.parse_direction_modes(str_, predicate, arity))
+        return modebs
 
 
 def literal_to_code(lit):
-    _, _, predicate, args = lit
+    predicate, _, args = lit
     args = map(lambda arg: chr(ord('A') + arg) if type(arg) == int else arg, args)
     return f"{predicate}({','.join(args)})"
 
@@ -61,9 +88,8 @@ def program_to_code(program):
 
 
 def is_recursive_clause(clause):
-    head_pred = clause[0][2]
     for literal in clause[1:]:
-        if literal[2] == head_pred:
+        if literal.predicate == clause[0].predicate:
             return True
     return False
 
