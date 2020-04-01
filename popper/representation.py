566780177cf71c99247bbfa0825f9b75acaa1789
polyphemus <rolfmorel@gmail.com>
2020-04-01 23:51:54 +0200

    New representation including helper functions

--- popper/representation.py
+++ popper/representation.py
@@ -13,17 +13,43 @@ from functools import reduce
 # where mode is an appropriate mode for each atom
 
 
+VAR_ANY = None # identifier for when the particular variable does not matter
+
+
 class Atom(namedtuple('Atom', ['predicate', 'mode', 'arguments'])):
-     @property
-     def arity(self):
-         return len(self.arguments)
+    def __str__(self):
+        args = map(lambda arg: f"V{arg}", self.arguments)
+        return f"{self.predicate}({','.join(args)})"
 
-     def __str__(self):
-         return f"{self.predicate}({','.join(self.arguments)})"
+    def __repr__(self): # strictly speaking wrong, but more useful for debugging
+        mode_args = (f"{m.value}V{arg}" for arg, m in zip(self.arguments, self.mode.arguments))
+        return f"{self.predicate}({','.join(mode_args)})"
 
-     def __repr__(self): # strictly speaking wrong, but more useful for debugging
-         mode_args = (f"{m.value}V{arg}" for arg, m in zip(self.arguments, self.mode.argument_modes))
-         return f"{self.predicate}({','.join(mode_args)})"
+    def to_code(self):
+        args = map(lambda arg: chr(ord('A') + arg) if type(arg) == int else arg,
+                   self.arguments)
+        return f"{self.predicate}({','.join(args)})"
+
+    def split_arguments(self):
+        ins, outs, unks = set(), set(), set()
+        
+        for idx, (mode, arg) in enumerate(zip(self.mode.arguments, self.arguments)):
+            if mode == ArgumentMode.Input: ins.add((idx,arg))
+            if mode == ArgumentMode.Output: outs.add((idx,arg))
+            if mode == ArgumentMode.Unknown: unks.add((idx,arg))
+        return ins, outs, unks
+
+    @property
+    def arity(self):
+        return len(self.arguments)
+
+    @property
+    def inputs(self):
+        return set(map(lambda idx_arg: idx_arg[1], self.split_arguments()[0]))
+
+    @property
+    def outputs(self):
+        return set(map(lambda idx_arg: idx_arg[1], self.split_arguments()[1]))
 
 
 class ArgumentMode(Enum):
@@ -32,27 +58,27 @@ class ArgumentMode(Enum):
     Unknown = '?'
 
 
-class ModeDecleration(namedtuple('ModeDecleration', ['predicate', 'argument_modes'])):
+class ModeDecleration(namedtuple('ModeDecleration', ['predicate', 'arguments'])):
     @property
     def arity(self):
         return len(self.argument_modes)
 
     def __str__(self):
         Out = ArgumentMode.Output
-        return self.predicate + "(" + ",".join("-" if mode == Out else "+" for mode in self.argument_modes) + ")"
+        return self.predicate + "(" + ",".join("-" if mode == Out else "+" for mode in self.arguments) + ")"
 
     @staticmethod
     def parse_direction_modes(str_, predicate, arity):
-        argument_modes = []
+        arguments = []
         for argument_idx in range(int(arity)):
             result = re.search(f"direction\({predicate}, *{argument_idx}, *([^\n ]*)\).", str_)
             if result and result.group(1) == 'out':
-                argument_modes.append(ArgumentMode.Output)
+                arguments.append(ArgumentMode.Output)
             elif result and result.group(1) == 'in':
-                argument_modes.append(ArgumentMode.Input)
+                arguments.append(ArgumentMode.Input)
             else: # NB, this case is used as a default as well
-                argument_modes.append(ArgumentMode.Unknown)
-        return __class__(predicate, tuple(argument_modes))
+                arguments.append(ArgumentMode.Unknown)
+        return __class__(predicate, tuple(arguments))
 
     @staticmethod
     def from_modeh(str_):
@@ -69,24 +95,6 @@ class ModeDecleration(namedtuple('ModeDecleration', ['predicate', 'argument_mode
         return modebs
 
 
-def literal_to_code(lit):
-    predicate, _, args = lit
-    args = map(lambda arg: chr(ord('A') + arg) if type(arg) == int else arg, args)
-    return f"{predicate}({','.join(args)})"
-
-
-def clause_to_code(clause):
-    lits = list(map(literal_to_code, clause))
-    return f"{lits[0]} :- {','.join(lits[1:])}"
-
-
-def program_to_code(program):
-    code_program = []
-    for clause in program:
-        code_program.append(clause_to_code(clause) + '.')
-    return code_program
-
-
 def is_recursive_clause(clause):
     for literal in clause[1:]:
         if literal.predicate == clause[0].predicate:
@@ -96,3 +104,38 @@ def is_recursive_clause(clause):
 
 def is_recursive_program(program):
     return reduce(lambda x, y: x or is_recursive_clause(y), program, False)
+
+
+def program_to_ordered_program(program):
+    def selection_closure(grounded_vars, literals):
+        if len(literals) == 0: return []
+        avail_lits = filter(
+                lambda lit: lit.inputs.union(lit.unknowns).issubset(grounded_vars),
+                literals)
+        selected_lit = next(avail_lits, None) # NB: selection is completely arbitrary
+        if selected_lit == None: raise ValueError(f"literals {literals} could not be grounded")
+        return [selected_lit] + \
+               selection_closure(grounded_vars.union(selected_lit.outputs),
+                                 literals.difference({selected_lit}))
+    ordered_clauses = []
+    for clause in program:
+        cl_id, head, body = clause
+        ordered_clauses.append((cl_id,
+                [head] + selection_closure(head.inputs, body.copy())))
+    return ordered_clauses
+
+
+def ordered_clause_to_code(clause):
+    _, head, body = clause
+    head, body = literal_to_code(head), map(str, body)
+    return f"{head} :- {','.join(body)}"
+
+
+def ordered_program_to_code(program):
+    code_program = []
+    for clause in program:
+        code_program.append(ordered_clause_to_code(clause) + '.')
+    return code_program
+
+def program_to_code(program):
+    return ordered_program_to_code(program_to_ordered_program(program))
