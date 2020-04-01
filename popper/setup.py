c930118b07b77b2a42d19553c164a13ca60c4ec1
polyphemus <rolfmorel@gmail.com>
2020-04-02 00:37:16 +0200

    Small change of reponsibility for parsing

--- popper/setup.py
+++ popper/setup.py
@@ -1,7 +1,6 @@
 from . import generate, test, constrain
 
 from .input import parse_examples
-from .representation import ModeDecleration
 from .util import TimeAccumulatingContext
 
 
@@ -9,16 +8,15 @@ def setup(mode_file, bk_file, examples_file,
           max_literals, eval_timeout, ground_constraints, no_pruning, debug):
     context = TimeAccumulatingContext()
     with context:
-        modeh = ModeDecleration.from_str(open(mode_file).read())
         pos_exs, neg_exs = parse_examples(examples_file)
 
         Generate = generate.Generate(mode_file, max_literals=max_literals, debug=debug)
         context.add_child('generate', Generate.context)
 
-        Test = test.Test(modeh, bk_file, pos_exs, neg_exs, eval_timeout, debug=debug)
+        Test = test.Test(Generate.modeh, bk_file, pos_exs, neg_exs, eval_timeout, debug=debug)
         context.add_child('test', Test.context)
 
-        Constrain = constrain.Constrain(modeh, ground=ground_constraints,
+        Constrain = constrain.Constrain(Generate.modeh, ground=ground_constraints,
                                         no_pruning=no_pruning, debug=debug)
         # NB: Constrain does not have a (time keeping) context
 
