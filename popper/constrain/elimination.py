08f67084ef7f9447fa890fe69183a95da1a829ff
polyphemus <rolfmorel@gmail.com>
2020-04-18 16:39:30 +0200

    TODO about elimination constraints and distinct clauses

--- popper/constrain/elimination.py
+++ popper/constrain/elimination.py
@@ -5,12 +5,14 @@ from .common import clause_to_asp_literals, asp_literals_for_distinct_clauses, \
 class EliminationMixin(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         
     def elimination_constraint(self, program):
         elim_lits = []
         for clause in program:
             elim_lits += clause_to_asp_literals(clause, self.ground)
         if not self.ground:
+            # TODO: can get rid of distinctness assertion, improving pruning
             elim_lits += asp_literals_for_distinct_clauses(program)
             elim_lits += asp_literals_for_distinct_variables(program)
 
