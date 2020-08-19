98e46a1e16e4544d1b3cf40ae063615505a5b120
polyphemus <rolfmorel@gmail.com>
2020-08-19 16:59:13 +0200

    Fix for 'python -m popper'

--- popper/__main__.py
+++ popper/__main__.py
@@ -1,9 +1,9 @@
 from .entry_point import main
-from .representation import program_to_prolog
+from . import representation
 
 program, context = main()
 if program:
-    for clause in program_to_prolog(program):
+    for clause in representation.program_to_prolog(program):
         print(clause, flush=True)
 else:
     print(None)
