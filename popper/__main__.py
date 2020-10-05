4a0697ef955c4f3fb22e64f04ba5508c5b14bb57
polyphemus <rolfmorel@gmail.com>
2020-10-05 13:56:43 +0100

    Import fix

--- popper/__main__.py
+++ popper/__main__.py
@@ -1,9 +1,9 @@
 from .entry_point import main
-from .representation import program_to_prolog
+from .representation import program_to_code
 
 program, context = main()
 if program:
-    for clause in program_to_prolog(program):
+    for clause in program_to_code(program):
         print(clause, flush=True)
 else:
     print(None)
