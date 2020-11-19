ddc027baf1d5ba8085a2a9a57ae701f8c4028e04
polyphemus <rolfmorel@gmail.com>
2020-11-19 22:48:46 +0000

    Update time keeping contexts in Generate

--- popper/generate/solver.py
+++ popper/generate/solver.py
@@ -8,9 +8,8 @@ class SolverMixin(object):
     def __init__(self, *args, **kwargs):
         self.context.add_child('solver')
         self.context.solver.add_child('setting_size')
-        self.context.solver.add_child('adding')
-        self.context.solver.add_child('grounding')
-        self.context.solver.add_child('solving')
+        self.context.solver.add_child('size_grounding')
+        self.context.solver.add_child('get_model')
         self.context.solver.add_child('conversion')
         super().__init__(*args, **kwargs)
 
@@ -20,15 +19,15 @@ class SolverMixin(object):
                 if atom.predicate == 'size' and truthiness:
                     self.solver.release(atom)
             # NB: might well attempt to reground the old parts of base as well
-            with self.context.solver.grounding:
+            with self.context.solver.size_grounding:
                 self.solver.ground(program_size=[size])
 
             self.solver.assign(Atom('size', (size,)), True)
 
     def get_program(self):
-        with self.context.solver:
+        with self.context.solver.get_model:
             model = self.solver.get_model()
-            if model:
-                with self.context.solver.conversion:
-                    return self.model_to_program(model)
-            return model
+        if model:
+            with self.context.solver.conversion:
+                return self.model_to_program(model)
+        return model
