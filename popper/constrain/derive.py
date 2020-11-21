e649fc58d5abc63b9bf0bddb1d266799d58806af
polyphemus <rolfmorel@gmail.com>
2020-11-21 11:14:16 +0000

    Save progress

--- popper/constrain/derive.py
+++ popper/constrain/derive.py
@@ -25,10 +25,10 @@ class DeriveMixin(object):
 
     def from_type(self, constraint_type, program):
         with self.context.derive:
-            if constraint_type == Gen: return self.specialization_constraint(program)
+            if constraint_type == Gen: return self.generalization_constraint(program)
             if constraint_type == Spec: return self.specialization_constraint(program)
-            if constraint_type == Elim: return self.specialization_constraint(program)
-            if constraint_type == Banish: return self.specialization_constraint(program)
+            if constraint_type == Elim: return self.elimination_constraint(program)
+            if constraint_type == Banish: return self.banish_constraint(program)
             assert False, "do not recognize '{constraint_type}' as a constraint type"
 
 
@@ -37,15 +37,30 @@ class DeriveMixin(object):
             if neg_outcome == All: 
                 neg_outcome = Some  # we do not distinguish between entailing some or all of the negative examples
 
+            inclusion_rules = self.derive_inclusion_rules(program)
+
             if (pos_outcome, neg_outcome) == (All, None_) or self.no_pruning:
                 # program was unfalsifiable, in which we can only prune it ...
                 # or we are instructed to prune no additional programs
                 constraints = [(Banish, self.banish_constraint(program))]
-            else:
-                if is_recursive_program(program): # FIXME: make this a check for separability
-                    constraints = self.general_constraints(program, pos_outcome, neg_outcome)
-                else:
-                    constraints = self.separable_constraints(program, pos_outcome, neg_outcome)
+            elif (pos_outcome, neg_outcome) == (All, Some):
+                constraints = [(Gen, self.generalization_constraint(program))]
+            elif (pos_outcome, neg_outcome) == (Some, None_):
+                constraints = [(Spec, self.specialization_constraint(program))]
+            elif (pos_outcome, neg_outcome) == (Some, Some):
+                constraints = [(Spec, self.specialization_constraint(program)),
+                               (Gen, self.generalization_constraint(program))]
+            else: 
+                assert pos_outcome == None_
+
+                inclusion_rules = chain(self.derive_inclusion_rules(program),
+                                        (self.program_inclusion_rule(program),))
+
+                constraints = [(Spec, self.specialization_constraint(program))] + \
+                              [(Elim, constraint) for constraint in self.elimination_constraint(program)]
+
+                if neg_outcome == Some:
+                    constraints += [(Gen, self.generalization_constraint(program))]
 
             named_constraints = []
             for type_, constraint in constraints:
@@ -53,37 +68,37 @@ class DeriveMixin(object):
                 self.id += 1
                 named_constraints.append((type_, name, constraint))
 
-            return chain(self.derive_inclusion_rules(program), named_constraints)
+            return chain(inclusion_rules, named_constraints)
 
 
-    def separable_constraints(self, program, pos_outcome, neg_outcome):
-        if (pos_outcome, neg_outcome) == (All, Some):
-            return [(Gen, self.generalization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (Some, None_):
-            return [(Spec, self.specialization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (Some, Some):
-            return [(Spec, self.specialization_constraint(program)),
-                    (Gen, self.generalization_constraint(program))]
-        # TODO: the elim constraints can be generalised so as to account the program being separable from other clauses
-        if (pos_outcome, neg_outcome) == (None_, None_):
-            return [(Spec, self.specialization_constraint(program))] + \
-                   [(Elim, self.elimination_constraint([clause])) for clause in program]
-        if (pos_outcome, neg_outcome) == (None_, Some):
-            return [(Spec, self.specialization_constraint(program))] + \
-                   [(Elim, self.elimination_constraint([clause])) for clause in program] + \
-                   [(Gen, self.generalization_constraint(program))]
-
-
-    def general_constraints(self, program, pos_outcome, neg_outcome):
-        if (pos_outcome, neg_outcome) == (All, Some):
-            return [(Gen, self.generalization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (Some, None_):
-            return [(Spec, self.specialization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (Some, Some):
-            return [(Spec, self.specialization_constraint(program)),
-                    (Gen, self.generalization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (None_, None_):
-            return [(Spec, self.specialization_constraint(program))]
-        if (pos_outcome, neg_outcome) == (None_, Some):
-            return [(Spec, self.specialization_constraint(program)),
-                    (Gen, self.generalization_constraint(program))]
+#    def separable_constraints(self, program, pos_outcome, neg_outcome):
+#        if (pos_outcome, neg_outcome) == (All, Some):
+#            return [(Gen, self.generalization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (Some, None_):
+#            return [(Spec, self.specialization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (Some, Some):
+#            return [(Spec, self.specialization_constraint(program)),
+#                    (Gen, self.generalization_constraint(program))]
+#        # TODO: the elim constraints can be generalised so as to account the program being separable from other clauses
+#        if (pos_outcome, neg_outcome) == (None_, None_):
+#            return [(Spec, self.specialization_constraint(program))] + \
+#                   [(Elim, self.elimination_constraint([clause])) for clause in program]
+#        if (pos_outcome, neg_outcome) == (None_, Some):
+#            return [(Spec, self.specialization_constraint(program))] + \
+#                   [(Elim, self.elimination_constraint([clause])) for clause in program] + \
+#                   [(Gen, self.generalization_constraint(program))]
+#
+#
+#    def general_constraints(self, program, pos_outcome, neg_outcome):
+#        if (pos_outcome, neg_outcome) == (All, Some):
+#            return [(Gen, self.generalization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (Some, None_):
+#            return [(Spec, self.specialization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (Some, Some):
+#            return [(Spec, self.specialization_constraint(program)),
+#                    (Gen, self.generalization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (None_, None_):
+#            return [(Spec, self.specialization_constraint(program))]
+#        if (pos_outcome, neg_outcome) == (None_, Some):
+#            return [(Spec, self.specialization_constraint(program)),
+#                    (Gen, self.generalization_constraint(program))]
--- popper/constrain/inclusion.py
+++ popper/constrain/inclusion.py
@@ -1,4 +1,4 @@
-from .common import clause_to_asp_literals, \
+from .common import clause_to_asp_literals, asp_literals_for_distinct_clauses, \
                     asp_literals_for_distinct_clause_variables
 
 from .data_types import RuleType
@@ -24,18 +24,32 @@ class InclusionMixin(object):
 
 
     def program_identifier(self, program, ground=None):
-        return '_'.join(sorted(self.clause_identifier(cl) for cl in program))
+        return '__'.join(sorted(self.clause_identifier(cl) for cl in program))
 
 
-    def inclusion_rule(self, clause):
+    def clause_inclusion_rule(self, clause):
         cl_handle = self.clause_identifier(clause)
         cl_id = str(clause[0]) if self.ground else "C"
 
         asp_lits = clause_to_asp_literals(clause, self.ground, cl_id=cl_id)
         if not self.ground:
-            asp_lits += asp_literals_for_distinct_clause_variables(clause, cl_id=cl_id)
+            asp_lits += asp_literals_for_distinct_clauses(program)
 
         return cl_handle, f"included_clause_{cl_handle}({cl_id}):-" + ",".join(asp_lits) + "."
+
+
+    def program_inclusion_rule(self, program):
+        cl_handle = self.program_identifier(program)
+
+        asp_lits = []
+        for i, cl_handle in enumerate(self.clause_identifier(cl) for cl in program):
+            clause_var = f"C{i}" if not self.ground else str(i)
+            asp_lits.append(f"included_clause_{cl_handle}({clause_var})")
+
+        if not self.ground:
+            asp_lits += asp_literals_for_distinct_clause_variables(clause, cl_id=cl_id)
+
+        return cl_handle, f"included_program_{program_handle}:-" + ",".join(asp_lits) + "."
 
     
     def derive_inclusion_rules(self, program):
