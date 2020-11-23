d8f49ec2cb098d809584cf905da23b455a101a7f
Andrew Cropper <andrew.cropper@gmail.com>
2020-11-23 17:56:09 +0000

    added min_clause

--- popper/generate/alan/alan.pl
+++ popper/generate/alan/alan.pl
@@ -91,3 +91,19 @@ before(C1,C2):-
     head_literal(C2,P,_,_),
     not recursive_clause(C1,P,A),
     recursive_clause(C2,P,A).
+
+count_lower(P,N):-
+    head_literal(_,P,_,_),
+    #count{Q : lower(Q,P)} == N.
+
+min_clause(C,N+1):-
+    recursive_clause(C,P,A),
+    count_lower(P,N).
+
+min_clause(C,N):-
+    head_literal(C,P,A,_),
+    not recursive_clause(C,P,A),
+    count_lower(P,N).
+
+%% #show before/2.
+%% #show min_clause/2.
