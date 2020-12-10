3f55ea174dfdae7cd83a9016e73775165808ca32
polyphemus <rolfmorel@gmail.com>
2020-12-10 09:25:49 +0000

    Only use one pred for included_clause and one for included_program

--- popper/constrain/banish.py
+++ popper/constrain/banish.py
@@ -11,7 +11,7 @@ class BanishMixin(object):
         for clause in program:
             cl_id, _, body = clause
             cl_handle = self.clause_identifier(clause)
-            banish_lits.append(f"included_clause_{cl_handle}({cl_id})")
+            banish_lits.append(f"included_clause({cl_handle},{cl_id})")
             banish_lits += [f"clause_size({cl_id},{len(body)})"]
         banish_lits += [f"not clause({len(program)})"]
         return ":-" + ",".join(banish_lits) + "."
--- popper/constrain/inclusion.py
+++ popper/constrain/inclusion.py
@@ -16,13 +16,12 @@ class InclusionMixin(object):
                      for var in atom.arguments)
             return f"{atom.predicate}" + "".join(vars_)
             
-        cl_id, head, body = clause
-        return "".join(map(atom_to_ident, [head] + sorted(body))) + \
-               (f"_cl{cl_id}" if ground else "")
+        return "".join(map(atom_to_ident,
+                           [clause.head] + sorted(clause.body)))
 
 
     def program_identifier(self, program, ground=None):
-        return 'program_' + '__'.join(sorted(self.clause_identifier(cl) for cl in program))
+        return 'prog_' + '_'.join(sorted(self.clause_identifier(cl) for cl in program))
 
 
     def clause_inclusion_rule(self, clause):
@@ -35,7 +34,7 @@ class InclusionMixin(object):
             asp_lits.append(f"{cl_id}>={clause.min_num}")
             asp_lits += asp_literals_for_distinct_clause_variables(clause, cl_id=cl_id)
 
-        return cl_handle, f"included_clause_{cl_handle}({cl_id}):-" + ",".join(asp_lits) + "."
+        return cl_handle, f"included_clause({cl_handle},{cl_id}):-" + ",".join(asp_lits) + "."
 
 
     def program_inclusion_rule(self, program):
@@ -45,7 +44,7 @@ class InclusionMixin(object):
         for cl in program:
             cl_handle = self.clause_identifier(cl)
             clause_var = f"C{cl.num}" if not self.ground else str(cl.num)
-            asp_lits.append(f"included_clause_{cl_handle}({clause_var})")
+            asp_lits.append(f"included_clause({cl_handle},{clause_var})")
 
         if not self.ground:
             for cl_num1, cl_nums in program.before.items():
@@ -54,4 +53,4 @@ class InclusionMixin(object):
 
             asp_lits += asp_literals_for_distinct_clauses(program)
 
-        return program_handle, f"included_{program_handle}:-" + ",".join(asp_lits) + "."
+        return program_handle, f"included_program({program_handle}):-" + ",".join(asp_lits) + "."
